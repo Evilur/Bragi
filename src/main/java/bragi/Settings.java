0b0b7646cb8a1ed64f33ed69e8949890bad567fa
Andrew-Flame <andrewflame@mail.ru>
2022-08-08 02:05:09 +0300

    Update Settings.java

--- src/main/java/bragi/Settings.java
+++ src/main/java/bragi/Settings.java
@@ -5,11 +5,11 @@ public class Settings {
 
     /* Метод для получения токена бота */
     public static String getBotToken() {
-        return "ODg4MjkyMzE4NjczNzgwNzY3.GM4EJw.IWGPne1f0MyEk4VRFqBa9pmNgQ2omJ7DkTKWSA";
+        return "Put your Discord bot's token here";
     }
 
     /* Метод для получения Deezer arl */
     public static String getDeezerArl() {
-        return "47cac401fd78b0f9403a1d6927ff52e5f9f9ae963a1f48a9c0676d010bab1c4465512c5600be76e81a3174138f224e68034ba6220c690d802cbd13fb009727f0a0f2bf01836af9cc45d9132bd17532d75749467004ef58ed0c4b324c63f6c71c";
+        return "Put your Deezer token here";
     }
 }
