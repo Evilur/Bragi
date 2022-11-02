33864738b946efc6f59108bec34b39c137732c4a
Andrew-Flame <andrewflame@mail.ru>
2022-11-02 14:36:45 +0300

    Remove the crutch, while the track was saved to disk, and then played back

--- src/main/java/bragi/Bragi.java
+++ src/main/java/bragi/Bragi.java
@@ -9,22 +9,12 @@ import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 
-import java.io.File;
 import java.util.HashMap;
-import java.util.Objects;
 
 public final class Bragi {
     public static HashMap<Guild, Player> Players = new HashMap<>();  //Инициализированные сервера
 
     public static void main(String[] args) throws Exception {
-        /* Очищаем мусор от предыдущей сессии */
-        for (File file : Objects.requireNonNull(new File("/tmp").listFiles())) {
-            if (file.getName().endsWith(".flac")) {
-                if (!file.delete())
-                    System.out.printf("Error while deleting file \"%s\"\n", file.getName());
-            }
-        }
-
         Settings.initialize();  //Инициализируем настройки
         DeezerMethods.initializeDeezer();  //Подключаемся к серверам Deezer и инициализируем клиент
 
--- src/main/java/bragi/core/plugins/lavaplayer/manager/deezer/DeezerAudioStream.java
+++ src/main/java/bragi/core/plugins/lavaplayer/manager/deezer/DeezerAudioStream.java
@@ -4,8 +4,6 @@ import com.sedmelluq.discord.lavaplayer.tools.Units;
 import com.sedmelluq.discord.lavaplayer.tools.io.*;
 import com.sedmelluq.discord.lavaplayer.track.info.AudioTrackInfoProvider;
 import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Collections;
@@ -21,117 +19,118 @@ import static org.apache.commons.codec.digest.DigestUtils.md5Hex;
 
 public final class DeezerAudioStream extends SeekableInputStream implements AutoCloseable {
     private final BufferedInputStream inputStream;  //Поток зашифрованных данных
-    private final FileOutputStream fileOutputStream;  //Поток записи в временный файл
-    private final FileInputStream fileInputStream;  //Поток чтеия из временного файла
-    private int currentChunk = 0;  //Номер текущего чанка (необходимо, потому что не все чанки нужно расшифровывать)
-    private int position = 0;  //Текущая позиция трека
-
     private final Cipher cipher;  //Шифр, с помощью которого будем расшифровывать трек
+    private byte[] chunk = new byte[2048];  //Чанк, куда будет записываться расшифрованная информация
+    private short localPosition = -1;  //Позиция байта в хранилище чанка
+    private short currentChunk = 0;  //Номер текущего чанка (необходимо, потому что не все чанки нужно расшифровывать)
+    private int totalPosition = 0;  //Текущая позиция трека
 
-    /* Основной конструктор */
+    /** Основной конструктор
+     * @param trackId Id Deezer трека
+     * @param trackUrl Url зашифрованного Deezer трека
+     */
     public DeezerAudioStream(String trackId, URL trackUrl) throws Exception {
         super(trackUrl.openConnection().getContentLength(), Units.CONTENT_LENGTH_UNKNOWN);
 
-        //Название временного файла
-        String filePath = "/tmp/" + trackId + ".flac";  //Устанавливаем название временного файла
-        this.fileOutputStream = new FileOutputStream(filePath);  //Инициализируем поток для записи во временный файл
-        this.fileInputStream = new FileInputStream(filePath);  //Инициализируем поток для чтения из веременного файла
-        this.inputStream = new BufferedInputStream(trackUrl.openStream());  //Создаем поток для чтения зашифрованных данных
+        /* Создаем поток для чтения зашифрованных данных */
+        this.inputStream = new BufferedInputStream(trackUrl.openStream());
 
         /* Работаем с шифром Blowfish */
-        SecretKeySpec key = new SecretKeySpec(GetKey(trackId).getBytes(), "Blowfish");  //Загружаем ключ для расшифровки данных
-        IvParameterSpec iv = new IvParameterSpec(new byte[] {0, 1, 2, 3, 4, 5, 6, 7});  //Устанавливаем iv пареметр
+        SecretKeySpec key = new SecretKeySpec(GetKey(trackId).getBytes(), "Blowfish");  //Ключ для расшифровки
+        IvParameterSpec iv = new IvParameterSpec(new byte[] {0, 1, 2, 3, 4, 5, 6, 7});  //Устанавливаем iv параметр
         this.cipher = Cipher.getInstance("Blowfish/CBC/NoPadding");  //Выбираем шифр
         this.cipher.init(Cipher.DECRYPT_MODE, key, iv);  //Инициализируем шифр с использованием ключа и iv параметра
     }
 
-    /* Метод для чтения одного байта */
+    /**
+     * Метод для чтения одного байта
+     * @return Либо байт, если он есть, либо -1, если поток уже пуст
+     */
     @Override
     public int read() throws IOException {
-        int result = this.fileInputStream.read();  //Подгружаем данные из потока
+        if (UpdateChunk()) {  //Если поток не закончился
+            /* Обновляем позицию */
+            this.localPosition = (short)(this.localPosition < 2048 ? this.localPosition + 1 : -1);
 
-        /* Если поток файла закончился, подгружаем туда новые данные */
-        if (result == -1) {
-            /* Если удаленный поток закончился, возвращаем -1, чтобы объявить о том, что поток закончился */
-            if (UpdateChunk())
-                result = this.fileInputStream.read();
-            else
-                result = clearTemp();
-        }
+            if (this.localPosition != -1) {  //Если чанк еще не закончился
+                this.totalPosition++;  //Обновляем позицию
+                int result = this.chunk[this.localPosition - 1];  //Здесь хранится результат
 
-        this.position++;  //Изменяем текущую позицию
-        return result;
+                /* Возвращаем результат */
+                if (result == -1) return 255;
+                else return result;
+            } else return this.read();  //Если чанк закончился
+        } else return -1;  //Если поток закончился
     }
 
-    /* Метод для чтения нескольких байтов сразу */
+    /** Метод для чтения нескольких байтов сразу
+     * @param buffer Буффер, в который будет записана информация
+     * @param offset Смещение, с которым будет считываться информация
+     * @param length Максимальное количество байтов для считывания
+     * @return Либо количество байт, если они есть, либо -1, если поток уже пуст
+     */
     @Override
     public int read(byte @NotNull[] buffer, int offset, int length) throws IOException {
-        int result = this.fileInputStream.read(buffer, offset, length);  //Подгружаем данные из потока
+        if (UpdateChunk()) {  //Если поток не закончился.
+            int result = 0;  //Переменная для вывода
+            for (int i1 = 0; i1 < length && this.localPosition < 2048; i1++, this.localPosition++) {
+                buffer[i1] = this.chunk[this.localPosition];
+                result++;
+            }
 
-        /* Если поток файла закончился, подгружаем туда новые данные */
-        if (result == -1) {
-            /* Если удаленный поток закончился, возвращаем -1, чтобы объявить о том, что поток закончился (удаляя временный файл) */
-            if (UpdateChunk())
-                result = this.fileInputStream.read(buffer, offset, length);
-            else
-                result = clearTemp();
-        }
+            /* Обновляем позицию */
+            this.localPosition = this.localPosition < 2048 ? this.localPosition : -1;
+            this.totalPosition += length;
 
-        this.position += result;  //Изменяем текущую позицию
-        return result;
+            return result;  //Возвращаем результат
+        } else  //Если поток закончился
+            return -1;
     }
 
-    /* Метод для подгрузки чанков */
+    /** Метод для подгрузки чанков
+     * @return true, если поток не окончен, false - в ином случае
+     * @throws IOException Выбрасывает исключение, если не удалось расшифровать чанк
+     * 2048 - константный размер чанка
+     */
     private boolean UpdateChunk() throws IOException {
-        int chunkSize = 2048;  //Стандартный размер чанка
+        /* Проверяем, надо ли подгружать новый чанк, или еще рано */
+        if (this.localPosition != -1) return true;
         int intervalChunk = 3;  //Интервал между чанками, которые расшифровывать не надо
-        byte[] chunk = new byte[chunkSize];  //Объявляем пустой чанк
-        int currentChunkSize = 0;  //Сюда будем запиисывать размер текущего чанка
+        int currentChunkSize = 0;  //Сюда будем записывать размер текущего чанка
 
-        /* Подгружаем чанк вручную, потому что родной метод работает как-то нестабильно (чанки различаются по размеру) */
-        for (int i = 0; i < chunkSize; i++)  {
+        /* Подгружаем чанк вручную, т.к. родной метод работает нестабильно */
+        for (int i = 0; i < 2048; i++)  {
             int value = this.inputStream.read();  //Получаем значение байта
 
             if (value != -1) {   //Если поток не закончился
-                chunk[i] = (byte)value;  //Кладем его в чанк
+                this.chunk[i] = (byte)value;  //Кладем его в чанк
                 currentChunkSize++;
-            }
-            else  //Если поток закончился
-                break;  //Выходим из цикла
+            } else break;  //Если поток закончился, выходим из цикла
         }
 
         /* Пробуем расшифровать чанк */
         try {
-            /* Получаетися так, что не все чанки нужно расшифровывать */
-            byte[] decryptedData;
-            if (this.currentChunk % intervalChunk == 0)
-                decryptedData = this.cipher.doFinal(chunk);
-            else
-                decryptedData = chunk;
-            this.fileOutputStream.write(decryptedData, 0, chunkSize);  //Записываем данные в файл
+            /* Получается так, что не все чанки нужно расшифровывать */
+            if (this.currentChunk % intervalChunk == 0) //Если надо, расшифровываем
+                this.chunk = this.cipher.doFinal(this.chunk);
         } catch (Exception ignore) {
+            System.out.println("Не удалось расшифровать чанк");
             return false;  //При ошибке прекращаем проигрывание
         }
 
         this.currentChunk++;
-        return currentChunkSize != 0;  //Если поток закончится, метод вернет значние false
+        this.localPosition++;
+        return currentChunkSize != 0;  //Если поток закончился, возвращаем false, иначе - true
     }
 
-    /* Метод для удаления временных файлов */
-    private int clearTemp() throws IOException {
-        /* Закрываем все потоки */
-        this.fileOutputStream.close();
-        this.fileInputStream.close();
-        this.inputStream.close();
-
-        return -1;
-    }
-
-    /* Метод для генерации ключа */
+    /** Метод для генерации ключа
+     * @param trackId Id трека Deezer
+     * @return Ключ для расшифровки
+     */
     private String GetKey(String trackId) {
         /* Получаем ключ для расшифровки трека */
-        String salt = "g4el58wc0zvf9na1";  //Соль, которая будет применяться вдальнейшем для расшифровки
-        String hash = md5Hex(String.valueOf(trackId));  //Получаем MD5 хэш-сумму идентефикатора трека
+        String salt = "g4el58wc0zvf9na1";  //Соль, которая будет применяться в дальнейшем для расшифровки
+        String hash = md5Hex(String.valueOf(trackId));  //Получаем MD5 хэш-сумму идентификатора трека
         String firstMD5Half = hash.substring(0, 16);  //Получаем первую половину хэш-суммы
         String secondMD5Half = hash.substring(16, 32);  //Получаем вторую половину хэш-суммы
         StringBuilder keyBuilder = new StringBuilder();  //В этой переменной будем хранить значение ключа
@@ -146,23 +145,13 @@ public final class DeezerAudioStream extends SeekableInputStream implements Auto
     }
 
     @Override
-    public long skip(long n) {
-        return Units.CONTENT_LENGTH_UNKNOWN;
-    }
-
+    public long skip(long n) { return Units.CONTENT_LENGTH_UNKNOWN; }
     @Override
-    public long getPosition() {
-        return this.position;
-    }
-
+    public long getPosition() { return this.totalPosition; }
     @Override
     protected void seekHard(long position) {    }
-
     @Override
-    public boolean canSeekHard() {
-        return false;
-    }
-
+    public boolean canSeekHard() { return false; }
     @Override
     public List<AudioTrackInfoProvider> getTrackInfoProviders() {
         return Collections.emptyList();
