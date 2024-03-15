a836ae6fe090bf69acf20dfa9685f9b7094f8eb5
Andrew-Flame <andrewflame@mail.ru>
2024-03-16 01:19:33 +0300

    Upgrade the join command

--- src/coms/join.cpp
+++ src/coms/join.cpp
@@ -27,24 +27,32 @@ void Join::Exec(dpp::cluster &bot, const dpp::message_create_t &event) {
 
 dpp::message Join::Message(dpp::cluster &bot, const dpp::snowflake guild_id, const dpp::snowflake user_id, dpp::discord_client* discord_c, 
 						   const dpp::snowflake channel_id) {
+	/* Get voice channels */
 	dpp::guild* guild = dpp::find_guild(guild_id);
 	dpp::channel* bot_vc = dpp::find_channel(guild->voice_members.find(bot.me.id)->second.channel_id);
 	dpp::channel* user_vc = dpp::find_channel(guild->voice_members.find(user_id)->second.channel_id);
+	
+	/* Check for the bot ready */
+	dpp::voiceconn* v_conn = discord_c->get_voice(guild_id);
+	bool is_ready = v_conn != nullptr && v_conn->is_ready();
 
 	/* If the user isn't in a voice channel */
 	if (user_vc == nullptr)
 		return Error::Force(DIC_ERROR_USER_NOT_IN_VOICE_CHANNEL, channel_id);
 
 	/* If the user and a bot already in the same channel */
-	if (bot_vc != nullptr && bot_vc->id == user_vc->id)
+	if (is_ready && bot_vc != nullptr && bot_vc->id == user_vc->id)
 		return Error::Soft(DIC_ERROR_ALREADY_IN_CURRENT_CHANNEL, channel_id);
 
-	/* If bot can not connect to the channel */
-	if (!user_vc->get_user_permissions(&bot.me).can(dpp::p_connect))
+	/* If bot can not connect to the channel or speak there */
+	if (!user_vc->get_user_permissions(&bot.me).can(dpp::p_connect) || !user_vc->get_user_permissions(&bot.me).can(dpp::p_speak))
 		return Error::Force(DIC_ERROR_PERMISSION_DENIED, channel_id);
+	
+	/* If bot in the voice channel we need to disconnect */
+	if (bot_vc != nullptr)
+		discord_c->disconnect_voice(guild_id);
 
 	/* If all is OK */
-	discord_c->disconnect_voice(guild_id);	//Disconnect the old voice channel
 	guild->connect_member_voice(user_id);  //Connect the new voice channel
 	return dpp::message(channel_id, std::format(DIC_JOINED, user_vc->name));
 }
---
+++ src/coms/leave.h
@@ -0,0 +1,12 @@
+#ifndef BRAGI_LEAVE_H
+#define BRAGI_LEAVE_H
+
+
+
+class Leave {
+
+};
+
+
+
+#endif
