aa569c2a1fbd0f013a92edae49f4f7d52f0f02de
Andrew-Flame <andrewflame@mail.ru>
2024-06-14 01:49:18 +0300

    Add a method for getting the url of the encoded track

--- src/client/deezer_client.cpp
+++ src/client/deezer_client.cpp
@@ -4,6 +4,7 @@
 #include "util/json.h"
 #include "util/logger.h"
 #include "player/track/deezer_track.h"
+#include "http/https_client.h"
 
 #include <iostream>
 
@@ -20,8 +21,7 @@ DeezerTrack* DeezerClient::Search(const std::string &request, const unsigned int
 
 	/* Send the request and init the json objects */
 	const char* json_string = HttpClient(_url_search_track, _headers, std::format(BODY_TEMPLATE_SEARCH_TRACK, request, start), "POST").ReadAll();
-	const Json json_global(json_string);
-	const Json json_results = json_global.Get("results");
+	const Json json_results = Json(json_string).Get("results");
 
 	/* Check for search results */
 	if (!json_results.Has("data")) return nullptr;
@@ -30,7 +30,7 @@ DeezerTrack* DeezerClient::Search(const std::string &request, const unsigned int
 	const Json json_track = json_results.Get("data").Get(0);
 
 	/* Create the track instance */
-	const std::string track_url = GetDecodedTrackUrl((std::string)json_track.Get("TRACK_TOKEN"));
+	const std::string track_url = GetEncodedTrackUrl((std::string)json_track.Get("TRACK_TOKEN"));
 	Logger::Debug(track_url);
 	DeezerTrack* result =
 			new DeezerTrack((std::string)json_track.Get("SNG_ID"), (std::string)json_track.Get("ALB_ID"), (std::string)json_track.Get("ART_ID"),
@@ -83,16 +83,23 @@ void DeezerClient::UpdateSession(const bool verbose) {
 	json_string = nullptr;
 }
 
-std::string DeezerClient::GetDecodedTrackUrl(const std::string &token, const Quality quality) {
-	/* Send the request */
-	const char* json_string = HttpClient(_url_get_decoded_track_url, _headers, std::format(BODY_TEMPLATE_GET_DECODED_TRACK_URL, _license_token, "FLAC", token),
-	                                     "POST").ReadAll();
+std::string DeezerClient::GetEncodedTrackUrl(const std::string &token, Quality quality) {
+	do {
+		/* Send the request */
+		const char* json_string =
+				HttpsClient(_url_get_decoded_track_url, _headers, std::format(BODY_TEMPLATE_GET_DECODED_TRACK_URL, _license_token, "FLAC", token),
+				            "POST").ReadAll();
 
-	std::cout << json_string;
-	Logger::Warn("Get the url");
-	exit(333);
+		Json json_media = Json(json_string).Get("data").Get(0).Get("media");
 
-	delete[] json_string;
-	json_string = nullptr;
-	return "";
+		/* If there is no track in such quality */
+		if (json_media.IsEmpty()) continue;
+
+		/* If the track in such quality exists */
+		delete[] json_string;
+		json_string = nullptr;
+		return (std::string)json_media.Get(0).Get("sources").Get(0).Get("url");
+	} while ((quality = (Quality)(quality - 1)) >= 0);
+
+	throw std::logic_error("Cannot find the encoded track url");
 }
