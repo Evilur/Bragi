1823782e23579fb0af821c875cb43798c1308b05
Andrew-Flame <andrewflame@mail.ru>
2024-06-14 01:04:08 +0300

    Add a temporary https client

---
+++ src/http/https_client.h
@@ -0,0 +1,21 @@
+#ifndef BRAGI_HTTPS_CLIENT_H
+#define BRAGI_HTTPS_CLIENT_H
+
+#include <string>
+
+/** !!!TEMPORARY!!! https client for debugging the bot */
+class HttpsClient final {
+public:
+	explicit HttpsClient(const std::string &url, const std::string &headers = "", const std::string &body = "", const char* requset_type = "GET");
+
+	/** Read all http document
+	 * @warning return data is an allocated memory so you need to free it later
+	 * @return a char aray with all http document data
+	 */
+	const char* ReadAll();
+
+private:
+	char* _data;
+};
+
+#endif
