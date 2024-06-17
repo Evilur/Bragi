62dc25316cace917d4b5f6bed99bad5fd4567ed3
Andrew-Flame <andrewflame@mail.ru>
2024-06-17 04:15:46 +0300

    Fix the json exception class

--- src/exception/json_exception.cpp
+++ src/exception/json_exception.cpp
@@ -4,10 +4,10 @@
 #include <format>
 
 JsonException::JsonException(const char* const json_data, const char* query) : _json_data(json_data), _wrong_query(
-		std::format("Can't find the object by key \"{}\" in the json data:", query)) { std::cout << query << '\n'; }
+		std::format("Can't find the object by key \"{}\" in the json data:", query)) { }
 
 JsonException::JsonException(const char* const json_data, const int index) : _json_data(json_data), _wrong_query(
-		std::format("Can't find the object by index {} in the json data:", index)) { std::cout << index << '\n'; }
+		std::format("Can't find the object by index {} in the json data:", index)) { }
 
 std::ostream &operator<<(std::ostream &stream, const JsonException &instance) {
 	return (stream << "Json object exception! " << instance._wrong_query << '\n' << instance._json_data);
