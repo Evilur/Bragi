2f9b24473459722e2bc8ddb988f17b723788cf98
Andrew-Flame <andrewflame@mail.ru>
2024-06-24 07:58:26 +0300

    Update the usage of the json class

--- src/client/deezer_client.cpp
+++ src/client/deezer_client.cpp
@@ -27,18 +27,18 @@ DeezerTrack* DeezerClient::Search(const std::string &query, const unsigned int s
 
 	/* Check for search results */
 	if (!json_results.Has("data")) return nullptr;
-	const unsigned short total = (unsigned short)json_results.Get("total");
+	const unsigned short total = (unsigned short)json_results["total"];
 	if (total == 0) return nullptr;
-	const Json json_track = json_results.Get("data").Get(0);
+	const Json json_track = json_results["data"][0];
 
 	/* Create the track instance */
-	//const std::string track_url = GetEncodedTrackUrl((std::string)json_track.Get("TRACK_TOKEN"));
+	//const std::string track_url = GetEncodedTrackUrl((std::string)json_track.["TRACK_TOKEN"]);
 	DeezerTrack* result =
-			new DeezerTrack((std::string)json_track.Get("SNG_ID"), (std::string)json_track.Get("ALB_ID"), (std::string)json_track.Get("ART_ID"),
-			                (std::string)json_track.Get("SNG_TITLE"), (std::string)json_track.Get("ALB_TITLE"), (std::string)json_track.Get("ART_NAME"),
-			                (std::string)json_track.Get("ALB_PICTURE"), (std::string)json_track.Get("ART_PICTURE"),
-			                (std::string)json_track.Get("DURATION"), "NOT IMPLEMENTED YET",
-			                (unsigned short)json_results.Get("total"), (unsigned short)json_results.Get("next"));
+			new DeezerTrack((std::string)json_track["SNG_ID"], (std::string)json_track["ALB_ID"], (std::string)json_track["ART_ID"],
+			                (std::string)json_track["SNG_TITLE"], (std::string)json_track["ALB_TITLE"], (std::string)json_track["ART_NAME"],
+			                (std::string)json_track["ALB_PICTURE"], (std::string)json_track["ART_PICTURE"],
+			                (std::string)json_track["DURATION"], "NOT IMPLEMENTED YET",
+			                (unsigned short)json_results["total"], (unsigned short)json_results["next"]);
 
 	/* Free the memory and return a result */
 	delete[] json_string;
@@ -51,18 +51,18 @@ void DeezerClient::UpdateSession(const bool verbose) {
 	/* Send the request and init the json objects */
 	const char* json_string = HttpClient(URL_UPDATE_SESSION, _headers).ReadAll();
 	const Json json_results = Json(std::strchr(json_string, '{')).Get("results");
-	const Json json_user = json_results.Get("USER");
+	const Json json_user = json_results["USER"];
 
 	/* Check the user for existing */
-	if ((unsigned int)json_user.Get("USER_ID") == 0) {
+	if ((unsigned int)json_user["USER_ID"] == 0) {
 		Logger::Fatal("Invalid Deezer ARL token");
 		exit(100);
 	}
 
 	/* Assign values to fields */
-	_session_id = (std::string)json_results.Get("SESSION_ID");
-	_license_token = (std::string)json_user.Get("OPTIONS").Get("license_token");
-	_session_timestamp = (unsigned long)json_results.Get("SERVER_TIMESTAMP");
+	_session_id = (std::string)json_results["SESSION_ID"];
+	_license_token = (std::string)json_user["OPTIONS"]["license_token"];
+	_session_timestamp = (unsigned long)json_results["SERVER_TIMESTAMP"];
 
 	/* Update the dependent urls */
 	_url_search_track = URL_TEMPLATE_SEARCH_TRACK + _session_id;
@@ -70,9 +70,9 @@ void DeezerClient::UpdateSession(const bool verbose) {
 
 	/* Get user data for logging */
 	if (verbose) {
-		const std::string user_name = (std::string)json_user.Get("BLOG_NAME");
-		const std::string user_email = (std::string)json_user.Get("EMAIL");
-		const std::string user_offer = (std::string)json_results.Get("OFFER_NAME");
+		const std::string user_name = (std::string)json_user["BLOG_NAME"];
+		const std::string user_email = (std::string)json_user["EMAIL"];
+		const std::string user_offer = (std::string)json_results["OFFER_NAME"];
 
 		/* Log the user data */
 		Logger::Info(std::format("Log in Deezer as \"{}\" <{}>", user_name, user_email));
@@ -91,7 +91,7 @@ std::string DeezerClient::GetEncodedTrackUrl(const std::string &token, Quality q
 				HttpsClient(_url_get_decoded_track_url, _headers, std::format(BODY_TEMPLATE_GET_DECODED_TRACK_URL, _license_token, "FLAC", token),
 				            "POST").ReadAll();
 
-		Json json_media = Json(json_string).Get("data").Get(0).Get("media");
+		Json json_media = Json(json_string)["data"][0]["media"];
 
 		/* If there is no track in such quality */
 		if (json_media.IsEmpty()) continue;
@@ -99,7 +99,7 @@ std::string DeezerClient::GetEncodedTrackUrl(const std::string &token, Quality q
 		/* If the track in such quality exists */
 		delete[] json_string;
 		json_string = nullptr;
-		return (std::string)json_media.Get(0).Get("sources").Get(0).Get("url");
+		return (std::string)json_media[0]["sources"][0]["url"];
 	} while ((quality = (Quality)(quality - 1)) >= 0);
 
 	throw std::logic_error("Cannot find the encoded track url");
