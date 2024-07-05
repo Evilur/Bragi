c93da308c58a7d5c9eb368208ba1d0835612d74e
Andrew-Flame <andrewflame@mail.ru>
2024-07-05 17:43:55 +0300

    Prepare code for testing

--- src/master.cpp
+++ src/master.cpp
@@ -17,6 +17,11 @@ int main() {
 	Settings::Init();
 	DeezerClient::Init();
 
+	Track* track = DeezerClient::Search("Korpiklaani Vodka");
+	track->Init();
+
+	return 0;
+
 	/* Create a bot cluster */
 	bot = new dpp::cluster(Settings::GetBotToken(), dpp::i_default_intents | dpp::i_message_content);
 
