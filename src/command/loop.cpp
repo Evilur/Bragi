e85231563ca4f4b9d6a23e56113c1a0ddeeb5f6b
Andrew-Flame <andrewflame@mail.ru>
2024-09-27 16:02:45 +0300

    Update the loop message command

--- src/command/loop.cpp
+++ src/command/loop.cpp
@@ -1,5 +1,6 @@
 #include "loop.h"
 #include "util/logger.h"
+#include "util/parser.h"
 
 void Loop::Exec(const dpp::slashcommand_t &event) {
 	/* Get the loop type slash command parameter */
@@ -14,18 +15,23 @@ void Loop::Exec(const dpp::slashcommand_t &event) {
 	/* Get the loop type in string */
 	const std::string loop_type_str = std::get<std::string>(loop_type_par);
 
-	/* Get the loop type */
-	GuildPlayer::LoopType loop_type;
-	if (loop_type_str == "loop_track") loop_type = GuildPlayer::LoopType::TRACK;
-	else if (loop_type_str == "loop_playlist") loop_type = GuildPlayer::LoopType::PLAYLIST;
-	else loop_type = GuildPlayer::LoopType::DISABLED;
-
 	/* Send the message to the channel */
-	event.reply(GuildPlayer::Get(event.command.guild_id)->Loop(event.command.channel_id, loop_type));
+	if (loop_type_str == "loop_track")
+		return event.reply(GuildPlayer::Get(event.command.guild_id)->Loop(event.command.channel_id, GuildPlayer::LoopType::TRACK));
+	else if (loop_type_str == "loop_playlist")
+		return event.reply(GuildPlayer::Get(event.command.guild_id)->Loop(event.command.channel_id, GuildPlayer::LoopType::PLAYLIST));
+	else
+		return event.reply(GuildPlayer::Get(event.command.guild_id)->Loop(event.command.channel_id, GuildPlayer::LoopType::DISABLED));
 }
 
 void Loop::Exec(const dpp::message_create_t &event, const std::string &argument) {
-	/* TODO: use argument */
-	/* Send the message to the channel */
-	event.send(GuildPlayer::Get(event.msg.guild_id)->Loop(event.msg.channel_id));
+	/* If the argument is empty */
+	if (argument.empty()) {
+		event.send(GuildPlayer::Get(event.msg.guild_id)->Loop(event.msg.channel_id));
+		return;
+	}
+
+	/* Get the loop type */
+	u_int8 loop_type_int = Parser::ToUInt8(argument.c_str() + argument.find_first_not_of(' '));
+	event.send(GuildPlayer::Get(event.msg.guild_id)->Loop(event.msg.channel_id, (GuildPlayer::LoopType)loop_type_int));
 }
