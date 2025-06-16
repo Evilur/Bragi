4332d45c2977f204a4b6f220d084dcee1c1e4ad7
Andrew Shershunov <mail@evilur.ru>
2025-06-16 13:59:11 +0300

    Feat: improve logger

--- src/util/logger.h
+++ src/util/logger.h
@@ -8,6 +8,8 @@
 #include <fstream>
 #include <cstdio>
 
+#define LOG_LEVEL 0
+
 #define TRACE_LOG(msg)
 #define DEBUG_LOG(msg)
 #define INFO_LOG(msg)
@@ -17,77 +19,60 @@
 
 #if LOG_LEVEL == 0
 #undef TRACE_LOG
-#define TRACE_LOG(msg) Logger::Log(msg, Logger::TRACE);
+#define TRACE_LOG(msg) Logger::Log(msg, Logger::TRACE, stdout);
 #endif
 #if LOG_LEVEL <= 1
 #undef DEBUG_LOG
-#define DEBUG_LOG(msg) Logger::Log(msg, Logger::DEBUG);
+#define DEBUG_LOG(msg) Logger::Log(msg, Logger::DEBUG, stdout);
 #endif
 #if LOG_LEVEL <= 2
 #undef INFO_LOG
-#define INFO_LOG(msg) Logger::Log(msg, Logger::INFO);
+#define INFO_LOG(msg) Logger::Log(msg, Logger::INFO, stdout);
 #endif
 #if LOG_LEVEL <= 3
 #undef WARN_LOG
-#define WARN_LOG(msg) Logger::Log(msg, Logger::WARN);
+#define WARN_LOG(msg) Logger::Log(msg, Logger::WARN, stdout);
 #endif
 #if LOG_LEVEL <= 4
 #undef ERROR_LOG
-#define ERROR_LOG(msg) Logger::Log(msg, Logger::ERROR);
+#define ERROR_LOG(msg) Logger::Log(msg, Logger::ERROR, stderr);
 #endif
 #if LOG_LEVEL <= 5
 #undef FATAL_LOG
-#define FATAL_LOG(msg) Logger::Log(msg, Logger::FATAL);
+#define FATAL_LOG(msg) Logger::Log(msg, Logger::FATAL, stderr);
 #endif
 
 /* Class for logging important messages */
 class Logger final {
 public:
-	/* Enumerator with log levels */
-	enum LogLevel : byte { TRACE, DEBUG, INFO, WARN, ERROR, FATAL };
+    /* Enumerator with log levels */
+    enum LogLevel : byte { TRACE, DEBUG, INFO, WARN, ERROR, FATAL };
 
-	/* Init the logger (for filesystem) */
-	static void Init();
-
-	template<typename T>
-	static void Trace(T message) { Log(message, TRACE); }
-
-	template<typename T>
-	static void Debug(T message) { Log(message, DEBUG); }
-
-	template<typename T>
-	static void Info(T message) { Log(message, INFO); }
-
-	template<typename T>
-	static void Warn(T message) { Log(message, WARN); }
-
-	template<typename T>
-	static void Error(T message) { Log(message, ERROR); }
-
-	template<typename T>
-	static void Fatal(T message) { Log(message, FATAL); }
-
-	/* Master log method */
-	template<typename T>
-	static void Log(T message, LogLevel log_level) {
-		/* Get the prefix. Format: '\e[35m[1970.01.01 00:00:00] DEBUG: ' */
-		char prefix[30];
-		sprintf(prefix, "%s[%s] %s", COLOR_CODE_STR[log_level], GetDate(), LOG_LEVEL_STR[log_level]);
+    /* Master log method */
+    template<typename T>
+    static void Log(T message, LogLevel log_level, FILE* stream) {
+        /* Get current time */
+        char time_buffer[20];
+        time_t current_time = time(nullptr);
+        tm* time_info = localtime(&current_time);
+        int offset = strftime(time_buffer, sizeof(time_buffer),
+                              "%Y.%m.%d %H:%M:%S", time_info);
 
 		/* Log the message */
-		std::cout << prefix << message << std::endl;
+        fprintf(stream, "%s[%s] %s %s\n",
+               COLOR_CODE_STR[log_level],
+               time_buffer,
+               LOG_LEVEL_STR[log_level],
+               message);
 	}
 
 private:
-	/* Date pointer (20 chars is enough to record the date and time: 2024.09.17 18:51:47 + \0) */
-	inline static char _current_date[20] = "1970.01.01 00:00:00";
-
-	/* Stock logging hideouts */
-	static constexpr char LOG_LEVEL_STR[6][8] = { "Trace: ", "Debug: ", "Info:  ", "Warn:  ", "Error: ", "Fatal: " };
-	static constexpr char COLOR_CODE_STR[6][6] = { "\e[37m", "\e[35m", "\e[34m", "\e[33m", "\e[31m", "\e[31m" };
-
-	/* Get the current formatted date */
-	static const char* GetDate();
+	static constexpr char LOG_LEVEL_STR[6][8] = {
+        "Trace: ", "Debug: ", "Info:  ", "Warn:  ", "Error: ", "Fatal: "
+    };
+	static constexpr char COLOR_CODE_STR[6][6] = {
+        "\e[37m", "\e[35m", "\e[34m", "\e[33m", "\e[31m", "\e[31m"
+    };
 };
 
 #endif
