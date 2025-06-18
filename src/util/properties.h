d200cb76c6af64d2b37eb88e7c303f655855f7a8
Andrew Shershunov <mail@evilur.ru>
2025-06-18 23:53:07 +0300

    fix(properties): init token pointers (nullptr)

--- src/util/properties.h
+++ src/util/properties.h
@@ -10,8 +10,8 @@ public:
 	static const char* ArlToken();
 
 private:
-    inline static const char* _bot_token;
-    inline static const char* _arl_token;
+    inline static const char* _bot_token = nullptr;
+    inline static const char* _arl_token = nullptr;
 
     static const char* InitToken(const char* var,
                                  const char* &token_ptr);
