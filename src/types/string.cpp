2e89645aae6f67786f4cc731ed90d0d0e3b729b2
Andrew Shershunov <mail@evilur.ru>
2025-07-14 00:01:18 +0300

    feat(String): Add '+ char' operator

--- src/types/string.cpp
+++ src/types/string.cpp
@@ -2,21 +2,29 @@
 
 #include <cstring>
 
-String::String(const unsigned long str_size) : _str(new char[str_size + 1]) { }
+String::String(const unsigned long str_size) :
+    _str(new char[str_size + 1]), _size(str_size) {
+    _str[str_size] = '\0';
+}
 
 String::String(const char* const str) : String(str, strlen(str)) { }
 
 String::String(const char* str, const unsigned long str_size) :
-    _str(new char[str_size + 1]) {
+    String(str_size) {
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wstringop-truncation"
     strncpy(_str, str, str_size);
-    _str[str_size] = '\0';
 #pragma GCC diagnostic pop
 }
 
 String::~String() {
     delete[] _str;
+}
+
+String String::operator+(const char symbol) const noexcept {
+    String result(_str, _size + 1);
+    result._str[_size] = symbol;
+    return result;
 }
 
 String::operator const char*() const {
--- src/types/string.hpp
+++ src/types/string.hpp
@@ -1,26 +1,33 @@
 #pragma once
 
 #include <string_view>
+#include <cstdio>
 
 class String final {
 public:
-    explicit String(unsigned long str_size);
+    String() noexcept = delete;
 
-    String(const char* str);
+    explicit String(unsigned long str_size) noexcept;
 
-    String(const char* str, unsigned long str_size);
+    String(const char* str) noexcept;
 
-    String(const String&) = delete;
-    String(String&&) = default;
+    String(const char* str, unsigned long str_size) noexcept;
 
-    ~String();
+    String(const String&) noexcept = delete;
 
-    operator const char*() const;
+    String(String&&) noexcept = default;
 
-    operator std::string_view() const;
+    ~String() noexcept;
+
+    String operator+(char symbol) const noexcept;
+
+    operator const char*() const noexcept;
+
+    operator std::string_view() const noexcept;
 
 private:
     char* const _str;
+    unsigned long _size;
 
 public:
     template<typename... Args>
