57fc5991ee41ed72638873adda3b8277db2dfb9f
Andrew Shershunov <mail@evilur.ru>
2025-08-21 16:59:54 +0300

    refactor: Make JsonException inherit from std::exception

--- src/exception/json_exception.h
+++ src/exception/json_exception.h
@@ -2,9 +2,8 @@
 #define BRAGI_JSON_EXCEPTION_H
 
 #include <exception>
-#include <string>
 
-class JsonException final {
+class JsonException final : std::exception {
 public:
         explicit JsonException(const char* json_data, const char* query);
 
